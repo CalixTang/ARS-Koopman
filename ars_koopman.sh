bc35eab7c40ce7e2829b6ef758c59ba0879461c7
CalixTang <calix.tang@gmail.com>
2024-05-23 14:03:45 -0400

    tmp commit

--- ars_koopman.sh
+++ ars_koopman.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-numbers=(1 2 4 8 16 32)
+numbers=(1 5 10 25 50 75 100 200 300 500)
 for i in "${numbers[@]}"
 do 
-    python ARS/code/ars_koopman.py --deltas_used $i --n_workers 8 > reports/pleaides-arsk-$i.out; 
+    python ARS/code/ars_koopman.py --policy_type eigenrelocate --num_modes $i --n_workers 8 > reports/pleaides-arsek-$i.out; 
 done
 
-numbers=(64 128 256 320)
-for i in "${numbers[@]}"
-do 
-    python ARS/code/ars.py --deltas_used $i --n_workers 8 > reports/pleaides-ars-$i.out; 
-done
+# numbers=(64 128 256 320)
+# for i in "${numbers[@]}"
+# do 
+#     python ARS/code/ars.py --deltas_used $i --n_workers 8 > reports/pleaides-ars-$i.out; 
+# done
 
     
