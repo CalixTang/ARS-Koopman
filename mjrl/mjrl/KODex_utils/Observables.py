7e98db680b05ac6e2efab3bfa87fffe0d928acd3
CalixTang <calix.tang@gmail.com>
2024-05-25 16:22:44 -0400

    refactor observables, new observables

--- mjrl/mjrl/KODex_utils/Observables.py
+++ mjrl/mjrl/KODex_utils/Observables.py
@@ -9,9 +9,56 @@ from mjrl.KODex_utils.gnn_networks import InteractionNetwork
 import copy
 import torch
 
-class KoopmanObservable(object):
+"""
+Base class for an observable. Should lift a state vector to a lifted state vector.
+"""
+class Observable(object):
     def __init__(self, num_envStates):
         self.num_states = num_envStates
+
+    """
+    Implementation of lifting function
+    """
+    def z(self, envState):
+        raise NotImplementedError
+    
+    """
+    Compute the size of lifted state given size of state 
+    """
+    def compute_observable(num_states):
+        raise NotImplementedError
+    
+    """
+    Compute the size of lifted state given size of state for this instance.
+    """
+    def compute_observables_from_self(self):
+        raise NotImplementedError
+
+class IdentityObservable(Observable):
+    def __init__(self, num_envStates):
+        super().__init__(num_envStates)
+
+    """
+    Implementation of lifting function
+    """
+    def z(self, envState):
+        return envState
+    
+    """
+    Compute the size of lifted state given size of state 
+    """
+    def compute_observable(num_states):
+        return num_states
+    
+    """
+    Compute the size of lifted state given size of state for this instance.
+    """
+    def compute_observables_from_self(self):
+        return self.num_states
+
+class LocomotionObservable(Observable):
+    def __init__(self, num_envStates):
+        super().__init__(num_envStates)
     
     def z(self, envState):  
         """
@@ -21,32 +68,36 @@ class KoopmanObservable(object):
         """
 
         #initialize lifted state arr
-        obs = np.zeros(KoopmanObservable.compute_observable(self.num_states))
+        obs = np.zeros(self.compute_observables_from_self())
         index = 0
 
         #for consistency, I keep the same order of functions as DraftedObservable as used in KODex/CIMER
 
         #x[i]
-        for i in range(self.num_states):
-            obs[index] = envState[i]
-            index += 1
+        obs[index : index + self.num_states] = envState[:]
+        index += self.num_states
 
         #x[i]^2
-        for i in range(self.num_states):
-            obs[index] = envState[i] ** 2
-            index += 1  
+        obs[index : index + self.num_states] = envState ** 2
+        index += self.num_states  
+
+        obs[index : index + self.num_states] = np.sin(envState)
+        index += self.num_states
+
+        obs[index : index + self.num_states] = np.cos(envState)
+        index += self.num_states
 
         #x[i] x[j] w/o repetition
-        for i in range(self.num_states):
-            for j in range(i + 1, self.num_states):
-                obs[index] = envState[i] * envState[j]
-                index += 1
+        # for i in range(self.num_states):
+        #     for j in range(i + 1, self.num_states):
+        #         obs[index] = envState[i] * envState[j]
+        #         index += 1
 
         # x[i]^2 x[j] w/ repetition - I removed the x[i]^3 here b/c this block includes x[i]^3
-        for i in range(self.num_states):
-            for j in range(self.num_states):
-                obs[index] = (envState[i] ** 2) * envState[j]
-                index += 1
+        # for i in range(self.num_states):
+        #     for j in range(self.num_states):
+        #         obs[index] = (envState[i] ** 2) * envState[j]
+        #         index += 1
         return obs
     
     def compute_observable(num_states):
@@ -56,16 +107,39 @@ class KoopmanObservable(object):
 
         #x[i], x[i]^2, x[i] x[j] w/o rep, x[i]^2 x[j]
 
-        
-        return 2 * num_states + (num_states * (num_states - 1) // 2) + num_states ** 2
+        return 4 * num_states
+        # return 2 * num_states + (num_states * (num_states - 1) // 2) + num_states ** 2
         
     
     def compute_observables_from_self(self):
         """
         Observation functions: original states, original states^2, cross product of hand states
         """
-        return KoopmanObservable.compute_observable(self.num_states)
+        return LocomotionObservable.compute_observable(self.num_states)
         
+
+"""
+A rewrite of DraftedObservable that is faster
+"""
+class ManipulationObservable(Observable):
+    def __init__(self, num_hand_states, num_obj_states):
+        super().__init__(num_hand_states + num_obj_states)
+        self.num_hand_states = num_hand_states
+        self.num_obj_states = num_obj_states
+    
+    
+    
+    def compute_observable(self, num_hand, num_obj):
+        """
+        Observation functions: original states, original states^2, cross product of hand states
+        """
+        return int(2 * (num_hand + num_obj) + (num_obj - 1) * num_obj / 2 + (num_hand - 1) * num_hand / 2 + num_hand + num_obj ** 2)  
+    
+    def compute_observables_from_self(self):
+        """
+        Observation functions: original states, original states^2, cross product of hand states
+        """
+        return ManipulationObservable.compute_observable(self.num_hand_states, self.num_obj_states)
 
 
 class DraftedObservable(object):
