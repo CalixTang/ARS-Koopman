4276b8050613a1645f4800b7eb96046b6787b54b
Calix Tang <calix.tang@gatech.edu>
2024-08-05 18:49:00 -0500

    complete koopman train rewrite, first run log and out files

--- koopman/koopman_train.py
+++ koopman/koopman_train.py
@@ -21,7 +21,7 @@ import numpy as np
 from tqdm import tqdm
 # from utils.gym_env import GymEnv
 # from utils.Observables import *
-from utils.Koopman_evaluation import *
+# from utils.Koopman_evaluation import *
 # from utils.Controller import *
 # from utils.quatmath import quat2euler, euler2quat
 # from utils.coord_trans import ori_transform, ori_transform_inverse
@@ -29,6 +29,7 @@ from datetime import datetime
 # import matplotlib.pyplot as plt
 # import matplotlib as mpl
 # from scipy.linalg import logm
+import scipy.linalg
 import scipy
 # import sys
 import os
@@ -46,40 +47,40 @@ from ARS.Observables import *
 # '''
 
 # MAIN =========================================================
-def main(demo_file, num_demo, velocity, save_matrix, matrix_file, folder_name):
-    num_demo = int(num_demo) # if num_demo != 0 -> we manually define the num of demo for testing the sample efficiency
-    Velocity = True if velocity == 'True' else False
-    save_matrix = True if save_matrix == 'True' else False  # save the Koopman matrix for Drafted method and trained model for MLP/GNN
+def main(demo_file, koopman_observable_choice = "LargeManipulationObservable", log_file = 'koopman_training_log.txt', out_file = 'koopman_matrix.npy'):
+    """
+    Trains weights for a koopman matrix given demonstrations, the number of demos to use, and the lifting function to use.
+
+        Params:
+            - demo_file                     The file with trajectories. Expected to be a 3d numpy array of dimensions (num_episodes, timesteps_per_episode, obs_dim)
+            - koopman_observable_choice     The choice of koopman observable to use. Refer to observables.py in ARS for valid choices.
+            - log_file                      Log file
+            - out_file                      Output file (for the K matrix)
+    """
+    # Velocity = True if velocity == 'True' else False
+    # save_matrix = True if save_matrix == 'True' else False  # save the Koopman matrix for Drafted method and trained model for MLP/GNN
     # if demo_file is None:
     #     demos = pickle.load(open('./demonstrations/'+env_name+'_demos.pickle', 'rb'))
     # else:
     #     demos = pickle.load(open(demo_file, 'rb'))
     
     # load demos
-    demos = np.load(demo_file, allow_pickle = True)
-
-    if num_demo == 0 or num_demo > len(demos):
-        num_demo = len(demos)
+    # demos = np.load(demo_file, allow_pickle = True)
    
     error_calc = 'median' # median error or mean error
-    unit_train = False
-    if len(folder_name) == 0: # we do not explicitly define the folder name for saving the models
-        folder_name = '/Relocate/'  # define the folder name for training
-        unit_train = True
-    if not os.path.exists(matrix_file):  # loading data for training
+    # unit_train = False
+    #load training data
 
-        #set up training data...
-        Training_data = np.load(demo_file)[: num_demo] # len(Training_data) = num_demo
-        num_handpos = len(Training_data[0][0]['handpos'])
-        num_handvel = len(Training_data[0][0]['handvel'])
-        num_objpos = len(Training_data[0][0]['objpos']) + len(Training_data[0][0]['objorient'])
-        num_objvel = len(Training_data[0][0]['objvel'])
-        dt = 1  # simulation time for each step, assuming it should be 1
-        num_obj = num_objpos + num_objvel
-        if Velocity: # hand velocities are also used as the original states
-            num_hand = num_handpos + num_handvel
-        else:
-            num_hand = num_handpos
+    #set up training data
+    training_episodes = np.load(demo_file) # len(training_episodes) = num_demo
+    num_episodes, timesteps_per_episode, obs_dim = training_episodes.shape
+
+    # num_handpos = len(training_episodes[0][0]['handpos'])
+    # num_handvel = len(training_episodes[0][0]['handvel'])
+    # num_objpos = len(training_episodes[0][0]['objpos']) + len(training_episodes[0][0]['objorient'])
+    # num_objvel = len(training_episodes[0][0]['objvel'])
+    dt = 1  # simulation time for each step, assuming it should be 1
+    # num_obj = num_objpos + num_objvel
     
     '''
     Train the koopman dynamics from demo data
@@ -87,58 +88,50 @@ def main(demo_file, num_demo, velocity, save_matrix, matrix_file, folder_name):
 
     # record the training errors
     # Drafted: error over num of used trajectories 
-    # MLP: error over num of training iterations
-    # GNN: error over num of training iterations
-    # Start the training process for each mode
-    if unit_train:
-        parent_dir = './Results/' + koopmanoption + folder_name
-        current_time = datetime.now().strftime("%d_%m_%Y_%H_%M_%S")
-        os.mkdir(os.path.join(parent_dir, current_time))
-        results_record = open('./Results/' + koopmanoption + folder_name + current_time + '/results.txt', 'w+')
-    else:
-        if not os.path.exists(os.path.join(folder_name, "koopman")):
-            os.mkdir(os.path.join(folder_name, "koopman"))
-        else:
-            shutil.rmtree(os.path.join(folder_name, "koopman"))   # Removes all the subdirectories!
-            os.mkdir(os.path.join(folder_name, "koopman"))
-        results_record = open(folder_name + "koopman" + '/results.txt', 'w+')
+
+    training_log_file = open(log_file, 'w+')
     start_time = time.time()
 
     #we only use "Drafted" observables for now (hardcoded lifting functions)
-    # if koopmanoption == "Drafted":
 
-    Koopman = DraftedObservable(num_hand, num_obj)
-    num_obs = Koopman.compute_observable(num_hand, num_obj)
+    koopman_obj = get_observable(koopman_observable_choice)(obs_dim)
+    num_obs = koopman_obj.compute_observables_from_self()
+
     print("number of observables:", num_obs)
     A = np.zeros((num_obs, num_obs))  
     G = np.zeros((num_obs, num_obs))
     ## loop to collect data
     print("Drafted koopman training starts!\n")
-    for k in tqdm(range(len(Training_data))):
-        if Velocity:
-            hand_OriState = np.append(Training_data[k][0]['handpos'], Training_data[k][0]['handvel'])
-        else:
-            hand_OriState = Training_data[k][0]['handpos']
-        obj_OriState = np.append(Training_data[k][0]['objpos'], np.append(Training_data[k][0]['objorient'], Training_data[k][0]['objvel']))
-        z_t = Koopman.z(hand_OriState, obj_OriState)  # initial states in lifted space
-        for t in range(len(Training_data[k]) - 1):
-            if Velocity:
-                hand_OriState = np.append(Training_data[k][t + 1]['handpos'], Training_data[k][t + 1]['handvel'])
-            else:
-                hand_OriState = Training_data[k][t + 1]['handpos']
-            obj_OriState = np.append(Training_data[k][t + 1]['objpos'], np.append(Training_data[k][t + 1]['objorient'], Training_data[k][t + 1]['objvel']))
-            z_t_1 = Koopman.z(hand_OriState, obj_OriState) # states in lifted space at next time step
-            # A and G are cumulated through all the demo data
+    for k in tqdm(range(num_episodes)):
+
+        #initial state
+        x_t = training_episodes[k, 0]
+        z_t = koopman_obj.z(x_t)
+        # if Velocity:
+        #     hand_OriState = np.append(training_episodes[k][0]['handpos'], training_episodes[k][0]['handvel'])
+        # else:
+        #     hand_OriState = training_episodes[k][0]['handpos']
+        # obj_OriState = np.append(training_episodes[k][0]['objpos'], np.append(training_episodes[k][0]['objorient'], training_episodes[k][0]['objvel']))
+        # z_t = koopman_obj.z(hand_OriState, obj_OriState)  # initial states in lifted space
+        for t in range(timesteps_per_episode - 1):
+            x_t_1 = training_episodes[k, t + 1]
+            z_t_1 = koopman_obj.z(x_t_1) # states in lifted space at next time step
+
+            # A and G are accumulated* through all the demo data
             A += np.outer(z_t_1, z_t)
             G += np.outer(z_t, z_t)
             z_t = z_t_1
-    M = len(Training_data) * (len(Training_data[0]) - 1)
+    M = len(training_episodes) * (len(training_episodes[0]) - 1)
     A /= M
     G /= M
     koopman_operator = np.dot(A, scipy.linalg.pinv(G)) # do not use np.linalg.pinv, it may include all large singular values
+    
+    #cont_koopman_operator looks like it was introduced to allow for us to solve with larger discrete time chunks. we don't use it
     # cont_koopman_operator = logm(koopman_operator) / dt
     cont_koopman_operator = koopman_operator
-    results_record.write("Training time is: %f seconds.\n"%(time.time() - start_time))
+
+
+    training_log_file.write("Training time is: %f seconds.\n"%(time.time() - start_time))
     # generate another matrix with similar matrix magnitude to verify the correctness of the learnt koopman matrix
     # we want to see that each element in the matrix does matter
     koopman_mean = np.mean(cont_koopman_operator)
@@ -154,29 +147,27 @@ def main(demo_file, num_demo, velocity, save_matrix, matrix_file, folder_name):
             # save the trained koopman matrix
 
     #always save the matrix
-    if unit_train:
-        np.save('./Results/Drafted' + folder_name + current_time + '/koopmanMatrix.npy', cont_koopman_operator)
-    else:
-        np.save(folder_name + "koopman" + '/koopmanMatrix.npy', cont_koopman_operator)
-        print("Koopman matrix is saved!\n")
+    np.save(out_file, cont_koopman_operator)
+    print("Koopman matrix is saved!\n")
     
     print("Koopman final testing starts!\n")
-    ErrorInLifted, ErrorInOriginal = koopman_evaluation(Koopman, cont_koopman_operator, Training_data, Velocity, num_hand, num_obj)
-    Fake_ErrorInLifted, Fake_ErrorInOriginal = koopman_evaluation(Koopman, Test_matrix, Training_data, Velocity, num_hand, num_obj)
+    ErrorInLifted, ErrorInOriginal = koopman_evaluation(koopman_obj, cont_koopman_operator, training_episodes) #testing on training...
+    Fake_ErrorInLifted, Fake_ErrorInOriginal = koopman_evaluation(koopman_obj, Test_matrix, training_episodes)
     print("Koopman final testing ends!\n")
+
     if error_calc == 'median': # compute the median 
         print("The final test accuracy in lifted space is: %f, and the accuracy in original space is: %f."%(np.median(ErrorInLifted), np.median(ErrorInOriginal)))
         print("The fake test accuracy in lifted space is: %f, and the fake accuracy in original space is: %f."%(np.median(Fake_ErrorInLifted), np.median(Fake_ErrorInOriginal)))
-        results_record.write("The final test accuracy in lifted space is: %f, and the accuracy in original space is: %f.\n"%(np.median(ErrorInLifted), np.median(ErrorInOriginal)))
-        results_record.write("The fake test accuracy in lifted space is: %f, and the fake accuracy in original space is: %f.\n"%(np.median(Fake_ErrorInLifted), np.median(Fake_ErrorInOriginal)))
+        training_log_file.write("The final test accuracy in lifted space is: %f, and the accuracy in original space is: %f.\n"%(np.median(ErrorInLifted), np.median(ErrorInOriginal)))
+        training_log_file.write("The fake test accuracy in lifted space is: %f, and the fake accuracy in original space is: %f.\n"%(np.median(Fake_ErrorInLifted), np.median(Fake_ErrorInOriginal)))
     else:
         print("The final test accuracy in lifted space is: %f, and the accuracy in original space is: %f."%(np.mean(ErrorInLifted), np.mean(ErrorInOriginal)))
         print("The fake test accuracy in lifted space is: %f, and the fake accuracy in original space is: %f."%(np.mean(Fake_ErrorInLifted), np.mean(Fake_ErrorInOriginal)))
-        results_record.write("The final test accuracy in lifted space is: %f, and the accuracy in original space is: %f.\n"%(np.mean(ErrorInLifted), np.mean(ErrorInOriginal)))
-        results_record.write("The fake test accuracy in lifted space is: %f, and the fake accuracy in original space is: %f.\n"%(np.mean(Fake_ErrorInLifted), np.mean(Fake_ErrorInOriginal)))
-    results_record.write("The number of demo used for experiment is: %d.\n"%(num_demo))
-    results_record.flush()
-    results_record.close()
+        training_log_file.write("The final test accuracy in lifted space is: %f, and the accuracy in original space is: %f.\n"%(np.mean(ErrorInLifted), np.mean(ErrorInOriginal)))
+        training_log_file.write("The fake test accuracy in lifted space is: %f, and the fake accuracy in original space is: %f.\n"%(np.mean(Fake_ErrorInLifted), np.mean(Fake_ErrorInOriginal)))
+    training_log_file.write("The number of demo used for experiment is: %d.\n"%(num_episodes))
+    training_log_file.flush()
+    training_log_file.close()
 
 
 # This function is taken from https://github.com/GT-STAR-Lab/KODex/blob/b058159f59baeff37437672e49f5d73406d4a5a6/Relocation/utils/Koopman_evaluation.py#L14. It is included in an import in the original koopman_train file, but I copy it into this file since I only use it locally.
@@ -224,21 +215,25 @@ def koopman_evaluation(koopman_object, koopman_matrix, eval_episodes):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--demo_file', type=str, required = True, help='demo file to load')
-    parser.add_argument('--num_demo', type=int, help='define the number of demo', default=0) 
+    parser.add_argument('--koopman_observable', type=str, default = 'LargeManipulationObservable')
+    parser.add_argument('--log_file', type=str, default = './koopman_training_log.txt')
+    parser.add_argument('--output_file', type=str, default='./koopmanMatrix.npy')
+    
+    # parser.add_argument('--num_demo', type=int, help='define the number of demo', default=0) 
     # parser.add_argument('--koopmanoption', type=str, help='Indicate the koopman choice (Drafted, MLP, GNN)', default='Drafted')   # We only use Drafted in my project
-    parser.add_argument('--velocity', type=str, help='If using hand velocity', default=None)
-    parser.add_argument('--save_matrix', type=str, help='If save the koopman matrix after training', default=None)
-    parser.add_argument('--matrix_file', type=str, help='If loading the saved matrix/model for test', default='')
-    parser.add_argument('--config', type=str, help='load the network info for MLP and GNN', default='')
-    parser.add_argument('--control', type=str, help='apply with a controller', default='')
-    parser.add_argument('--error_type', type=str, help='define how to calculate the errors', default='demo') # two options: demo; goal
-    parser.add_argument('--visualize', type=str, help='define whether or not to visualze the manipulation results', default='') # two options: demo; goal
-    parser.add_argument('--unseen_test', type=str, help='define if we generate unseen data for testing the learned dynamics model', default='') # generate new testing data
-    parser.add_argument('--rl_policy', type=str, help='define the file location of the well-trained policy', default='') 
-    parser.add_argument('--folder_name', type=str, help='define the location to put trained models', default='') 
+    # parser.add_argument('--velocity', type=str, help='If using hand velocity', default=None)
+    # parser.add_argument('--save_matrix', type=str, help='If save the koopman matrix after training', default=None)
+    # parser.add_argument('--matrix_file', type=str, help='If loading the saved matrix/model for test', default='')
+    # parser.add_argument('--config', type=str, help='load the network info for MLP and GNN', default='')
+    # parser.add_argument('--control', type=str, help='apply with a controller', default='')
+    # parser.add_argument('--error_type', type=str, help='define how to calculate the errors', default='demo') # two options: demo; goal
+    # parser.add_argument('--visualize', type=str, help='define whether or not to visualze the manipulation results', default='') # two options: demo; goal
+    # parser.add_argument('--unseen_test', type=str, help='define if we generate unseen data for testing the learned dynamics model', default='') # generate new testing data
+    # parser.add_argument('--rl_policy', type=str, help='define the file location of the well-trained policy', default='') 
+    # parser.add_argument('--folder_name', type=str, help='define the location to put trained models', default='') 
     args = parser.parse_args()
 
-    main()
+    main(args.demo_file, args.koopman_observable, args.log_file, args.output_file)
 
 
 # class Observable(object):
